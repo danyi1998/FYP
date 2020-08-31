1f2c2e2c6e21a40341c681ad88cf579441bceb6d
danyi1998 <46101155+danyi1998@users.noreply.github.com>
2020-08-31 09:26:12 +0800

    Add files via upload

--- transcriptomic.R
+++ transcriptomic.R
@@ -4,3 +4,8 @@ transcriptomic_df <- as.data.frame(transcriptomic_df)
 
 transcriptomic_df <- transcriptomic_df[vapply(transcriptomic_df, function(x) length(unique(x)) > 1, logical(1L))] 
 
+principal_comps <- prcomp(transcriptomic_df, center = TRUE, scale. = TRUE) 
+
+pc_transcriptomic_df <- principal_comps$x
+pc_transcriptomic_df <- as.data.frame(pc_transcriptomic_df)
+
