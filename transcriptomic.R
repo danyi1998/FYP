8972d369b7ca15b36f6fa30d8a0df21fed9a3381
danyi1998 <46101155+danyi1998@users.noreply.github.com>
2020-09-30 00:44:01 +0800

    Add files via upload

--- transcriptomic.R
+++ transcriptomic.R
@@ -9,3 +9,25 @@ principal_comps <- prcomp(transcriptomic_df, center = TRUE, scale. = TRUE)
 pc_transcriptomic_df <- principal_comps$x
 pc_transcriptomic_df <- as.data.frame(pc_transcriptomic_df)
 
+pc_transcriptomic_df_10 <- pc_transcriptomic_df[,1:10] 
+
+standardised_pc_transcriptomic_df_10 <- scale(pc_transcriptomic_df_10)
+standardised_pc_transcriptomic_df_10 <- as.data.frame(standardised_pc_transcriptomic_df_10)
+
+library(factoextra)
+library(cluster)
+
+set.seed(123)
+fviz_nbclust(standardised_pc_transcriptomic_df_10, kmeans, iter.max = 30, nstart = 25, method = "wss")
+set.seed(123)
+fviz_nbclust(standardised_pc_transcriptomic_df_10, kmeans, iter.max = 30, nstart = 25, method = "silhouette")
+
+set.seed(123)
+kmeans_result <- kmeans(standardised_pc_transcriptomic_df_10, 8, nstart = 25)
+
+fviz_cluster(kmeans_result, data = standardised_pc_transcriptomic_df_10[, 1:2])
+
+cluster_allocation <- kmeans_result$cluster 
+cluster_allocation <- data.frame(cluster_allocation)
+
+standardised_pc_transcriptomic_df_10_extracolumns <- cbind(standardised_pc_transcriptomic_df_10, cluster_allocation)
