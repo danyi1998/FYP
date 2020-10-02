928313e21fbf44ede77735c39c292252a2b57268
danyi1998 <46101155+danyi1998@users.noreply.github.com>
2020-10-02 08:10:53 +0800

    Add files via upload

--- transcriptomic.R
+++ transcriptomic.R
@@ -8,6 +8,9 @@ principal_comps <- prcomp(transcriptomic_df, center = TRUE, scale. = TRUE)
 
 summary(principal_comps)
 
+gene_loadings <- principal_comps$rotation
+gene_loadings <- gene_loadings[, 1:2]
+
 pc_transcriptomic_df <- principal_comps$x
 pc_transcriptomic_df <- as.data.frame(pc_transcriptomic_df)
 
@@ -56,3 +59,20 @@ metadata$age <- as.numeric(metadata$age)
 standardised_pc_transcriptomic_df_2_extracolumns <- cbind(standardised_pc_transcriptomic_df_2_extracolumns, metadata)
 
 standardised_pc_transcriptomic_df_2_extracolumns <- standardised_pc_transcriptomic_df_2_extracolumns[standardised_pc_transcriptomic_df_2_extracolumns$status != "Control", ]
+
+cluster_age <- plot(standardised_pc_transcriptomic_df_2_extracolumns$cluster_allocation, standardised_pc_transcriptomic_df_2_extracolumns$age, type="p", xlab="cluster", ylab="age")
+
+cluster_sex <- aggregate(standardised_pc_transcriptomic_df_2_extracolumns, by=list(standardised_pc_transcriptomic_df_2_extracolumns$cluster_allocation, standardised_pc_transcriptomic_df_2_extracolumns$sex), FUN=length)
+cluster_sex <- cluster_sex[, 1:3]
+colnames(cluster_sex) <- c("cluster","sex", "count")
+cluster_sex_plot <- plot(cluster_sex$cluster, cluster_sex$count, type="p", xlab="cluster", ylab="count", main="Cluster-Sex" , col=ifelse(cluster_sex$sex=="F",'red','blue'))
+
+cluster_lithium <- aggregate(standardised_pc_transcriptomic_df_2_extracolumns, by=list(standardised_pc_transcriptomic_df_2_extracolumns$cluster_allocation, standardised_pc_transcriptomic_df_2_extracolumns$lithium), FUN=length)
+cluster_lithium <- cluster_lithium[, 1:3]
+colnames(cluster_lithium) <- c("cluster","lithium", "count")
+cluster_lithium_plot <- plot(cluster_lithium$cluster, cluster_lithium$count, type="p", xlab="cluster", ylab="count", main="Cluster-Lithium" , col=ifelse(cluster_lithium$lithium=="1",'red','blue'))
+
+cluster_tobacco <- aggregate(standardised_pc_transcriptomic_df_2_extracolumns, by=list(standardised_pc_transcriptomic_df_2_extracolumns$cluster_allocation, standardised_pc_transcriptomic_df_2_extracolumns$tobacco), FUN=length)
+cluster_tobacco <- cluster_tobacco[, 1:3]
+colnames(cluster_tobacco) <- c("cluster","tobacco", "count")
+cluster_tobacco_plot <- plot(cluster_tobacco$cluster, cluster_tobacco$count, type="p", xlab="cluster", ylab="count", main="Cluster-Tobacco" , col=ifelse(cluster_tobacco$tobacco=="1",'red','blue'))
